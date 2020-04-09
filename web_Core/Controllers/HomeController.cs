b251cbd2024ce04280fea31efdabc329b0213c07
Nikolai <A6oltyc@list.ru>
2020-04-09 13:23:42 +0300

    Первый коммит
    тестим файлик )

---
+++ web_Core/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using web_Core.Models;
+
+namespace web_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<Computer> comps = new List<Computer>();
+        static HomeController()
+        {
+            comps.Add(new Computer { Id = 1, Name = "Apple II", Company = "Apple", Year = 1977 });
+            comps.Add(new Computer { Id = 2, Name = "Macintosh", Company = "Apple", Year = 1983 });
+            comps.Add(new Computer { Id = 3, Name = "IBM PC", Company = "IBM", Year = 1981 });
+            comps.Add(new Computer { Id = 4, Name = "Altair", Company = "MITS", Year = 1975 });
+        }
+        public ActionResult Index()
+        {
+            return View(comps);
+        }
+    
+    }
+}
---
+++ web_Core/Models/Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_Core.Models
+{
+    public class Computer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public int Year { get; set; }
+
+    }
+}
