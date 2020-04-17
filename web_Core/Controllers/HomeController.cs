c585ffa739bc069d5890257ad5397cb7e5861a39
nik <A6oltyc@list.ru>
2020-04-17 10:57:18 +0300

    yeeeee

--- web_Core/Controllers/HomeController.cs
+++ web_Core/Controllers/HomeController.cs
@@ -11,22 +11,21 @@ namespace web_Core.Controllers
 {
     public class HomeController : Controller
     {
-        static List<Computer> comps = new List<Computer>();
         static HomeController()
         {
-            comps.Add(new Computer { Id = 1, Name = "Apple II", Company = "Apple", Year = 1977 });
-            comps.Add(new Computer { Id = 2, Name = "Macintosh", Company = "Apple", Year = 1983 });
-            comps.Add(new Computer { Id = 3, Name = "IBM PC", Company = "IBM", Year = 1981 });
-            comps.Add(new Computer { Id = 4, Name = "Altair", Company = "MITS", Year = 1975 });
+;
         }
         public ActionResult Index()
         {
-            return View(comps);
+            return View();
         }
 
-        public ActionResult Details()
+        public ActionResult Authorization()
         {
-           
+            return View();
+        }
+        public ActionResult HelpDesk()
+        {
             return View();
         }
 
